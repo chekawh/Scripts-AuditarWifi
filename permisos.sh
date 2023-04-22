caaa2bd25d7aa94b03de73a316324d0364561ca6
ChekaWH <45906536+chekawh@users.noreply.github.com>
2023-04-22 20:43:37 +0100

    Add files via upload

---
+++ permisos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\n"
+echo "*******************************************"
+echo "***                                     ***"
+echo "***         PERMISOS  ARCHIVOS          ***"
+echo "***                                     ***"
+echo "***   ​ᴄᴏᴘʏʀɪɢᴛʜ@𝟮𝟬𝟮𝟯        𝑪𝒉𝒆𝒌𝒂𝑾𝑯     ***"
+echo "***                                     ***"
+echo "*******************************************"
+
+echo -e "\n"
+chmod +755 auditarwifi.sh
+chmod +755 deauth.sh
+chmod +755 iniciar.sh
+chmod +755 resetear.sh
+
+echo "➤➤➤ Los permisos se han asignado de manera correcta."
+echo -e "\n"
